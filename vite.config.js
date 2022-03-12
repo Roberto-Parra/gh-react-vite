b680d795ddce65ed8de2336626ae5600bd32858d
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-03-12 00:24:57 -0300

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://roberto-parra.github.io/',
+  base: 'https://roberto-parra.github.io/gh-react-vite/',
   plugins: [react()]
 })
